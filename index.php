b61b6f431b997dc0c2cf785e55bb130365c50d8b
highb33kay <hiibeekayvibe@gmail.com>
2023-01-25 05:26:41 +0100

    test files

---
+++ index.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php include('templates/header.php'); ?>
+    <link href="templates/styles.css" rel="stylesheet">
+    <?php echo 'hello' ?>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head> 
+    <meta charset="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Header</title>
+
+    <!-- font awesome icons -->
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <!-- link style sheet to php page -->
+    <link href="styles.css" rel="stylesheet">
+    
+  </head>
+  <body>
+    <header>
+      <nav class="nav">
+        <ul>
+            <li>Home</li>
+            <li>Title</li>
+            <button class="btn primary"> Continue </button>
+        </ul>
+      </nav>
+    </header>
